6c5ebcd4e29c65a81266efa9e333154f04d2220a
hugofpiani <hugo.fontolan@gmail.com>
2025-08-13 09:25:55 -0300

    Add comments and improve code formatting in script.js
    Added explanatory comments throughout the Firestore integration code for better clarity. Improved code formatting and structure for readability, including spacing and indentation adjustments.

--- sections/21_firebase_9/firebase_9/src/script.js
+++ sections/21_firebase_9/firebase_9/src/script.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, getDocs, onSnapshot, addDoc, deleteDoc, doc, query, where, serverTimestamp, orderBy } from "firebase/firestore";
 
+// Access configuration for the Firestore database.
 const firebaseConfig = {
   apiKey: "AIzaSyB1qwQ5Tb16zvHCY-5eZ1NPjsbzo941Wfk",
   authDomain: "fir-dojo-974e8.firebaseapp.com",
@@ -11,15 +12,19 @@ const firebaseConfig = {
   measurementId: "G-57H72QZ5Y8"
 };
 
+// Initializing the Firestore instance:
 initializeApp(firebaseConfig);
 
+// Getting a reference to both the database in the constant 'db' and the collection 'books' in the constant 'colRef':
 const db = getFirestore();
 
 const colRef = collection(db, 'books')
 
+// Creating a query for querying the database:
 // const q = query(colRef, where('author', '==', 'Claudia Renata'), orderBy('title', 'desc'));
 const q = query(colRef, orderBy('createdAt'));
 
+// Fetching data:
 // getDocs(colRef)
 //     .then(snapshot => {
 //       let books = []; 
@@ -32,15 +37,17 @@ const q = query(colRef, orderBy('createdAt'));
 //       console.log(err.message);
 //     });
 
+// Real-time listener for database changes
 // onSnapshot(colRef, snapshot => {
 onSnapshot(q, snapshot => {
-let books = []; 
-      snapshot.docs.forEach(doc => {
-        books.push({...doc.data(), id: doc.id});
-      });
-      console.log(books);
-  })
+  let books = [];
+  snapshot.docs.forEach(doc => {
+    books.push({ ...doc.data(), id: doc.id });
+  });
+  console.log(books);
+})
 
+// Adding documents:
 const addBook = document.querySelector('.add');
 addBook.addEventListener('submit', event => {
   event.preventDefault();
@@ -50,17 +57,20 @@ addBook.addEventListener('submit', event => {
     author: addBook.author.value,
     createdAt: serverTimestamp(),
   })
-  .then(() => {
-    addBook.reset();
-  });
+    .then(() => {
+      addBook.reset();
+    });
 })
 
+// Deleting documents:
 const deleteBook = document.querySelector('.delete');
 deleteBook.addEventListener('submit', event => {
   event.preventDefault();
-  
+
+  // Create a reference to the specific document using its ID
   const docRef = doc(db, 'books', deleteBook.id.value);
 
+  // Delete the document from Firestore
   deleteDoc(docRef)
     .then(() => {
       deleteBook.reset();
